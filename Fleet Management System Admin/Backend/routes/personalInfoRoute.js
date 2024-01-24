869f233c13e5746f87418047a7d092604c68346f
Supremo1717 <ralphmatthewmanabat@gmail.com>
2024-01-24 12:26:21 +0800

    picture

--- Fleet Management System Admin/Backend/routes/personalInfoRoute.js
+++ Fleet Management System Admin/Backend/routes/personalInfoRoute.js
@@ -7,7 +7,7 @@ const db = require('../database/connection')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'images')
+        cb(null, './images')
     },
     filename: (req, file, cb) => {
         cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
