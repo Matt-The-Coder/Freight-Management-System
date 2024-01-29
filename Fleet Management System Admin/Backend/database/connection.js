ccf993e561c142d552599ac2ffb44ba64ad476f1
Supremo1717 <ralphmatthewmanabat@gmail.com>
2024-01-29 18:36:32 +0800

    created real-time map

--- Fleet Management System Admin/Backend/database/connection.js
+++ Fleet Management System Admin/Backend/database/connection.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    password:'',
-    database: "fleet",
-    port: "3307"
-})
 // const db = mysql.createPool({
-//     host: 'bjxwp9zadzdyfnlsid8b-mysql.services.clever-cloud.com',
-//     user:'uyo3diweamt0qrca',
-//     password:'SSSLkR8oQLwUFVl0kyWU',
-//     database: "bjxwp9zadzdyfnlsid8b",
-//     port: "3306"
+//     host: 'localhost',
+//     user:'root',
+//     password:'',
+//     database: "fleet",
+//     port: "3307"
 // })
+const db = mysql.createPool({
+    host: 'bjxwp9zadzdyfnlsid8b-mysql.services.clever-cloud.com',
+    user:'uyo3diweamt0qrca',
+    password:'SSSLkR8oQLwUFVl0kyWU',
+    database: "bjxwp9zadzdyfnlsid8b",
+    port: "3306"
+})
 
 
 module.exports = async (query) => {
